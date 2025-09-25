577ed5610e6165f8cf42876c9c5a99c1ca872df2
Subasri-Balasubramaniyan <subasribalu111@gmail.com>
2025-09-25 17:50:41 +0530

    Add files via upload

---
+++ basics.js
@@ -0,0 +1,30 @@
+// Variables
+var name = "Subasri";   // can be redeclared
+let age = 22;           // block scoped
+const country = "India"; // cannot be changed
+
+console.log(name, age, country);
+
+// Constant
+let str = "Hello";      // string
+let num = 25;           // number
+let isStudent = true;   // boolean
+let notDefined;         // undefined
+let empty = null;       // null
+let arr = [1, 2, 3];    // array
+let obj = { name: "Subasri", age: 22 }; // object
+console.log(str,num,isStudent,notDefined,empty,arr,obj)
+
+// Basic operators
+let x = 10, y = 5;
+
+// Arithmetic
+console.log(x + y); // 15
+console.log(x * y); // 50
+
+// Comparison
+console.log(x > y);  // true
+console.log(x === 10); // true
+
+// Logical
+console.log(x > 5 && y < 10); // true
---
+++ functions.js
@@ -0,0 +1,20 @@
+// Normal function
+function greet(name){
+    return "Hello"+name;
+}
+console.log(greet("Subasri"))
+
+
+const add=(a,b)=>a+b;
+console.log(add(5,3))
+
+// Conditional statements
+let marks = 80;
+
+if (marks >= 90) {
+  console.log("Grade A");
+} else if (marks >= 75) {
+  console.log("Grade B");
+} else {
+  console.log("Grade C");
+}
